4546e9f9f9dbab9cdee0072d2cc44d9d3c42fc03
osman <osmnlhn@gmail.com>
2023-12-11 18:51:08 +0000

    son teslim background color

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,54 @@
 import React from "react";
+import { formatDistanceToNow, parseISO,differenceInHours } from 'date-fns';
+import { tr } from "date-fns/locale";
+import './app.css'
 
 const Task = ({ taskObj, onComplete }) => {
+  
+  const d = new Date();
+  const deadlineDate = typeof taskObj.deadline === 'string'
+  ? parseISO(taskObj.deadline)
+  : taskObj.deadline;
+  const distanceToNow = formatDistanceToNow(deadlineDate, { 
+    addSuffix: true,
+    locale:tr, 
+  });
+
+  const difference = differenceInHours(deadlineDate, d);
+
+  //const className=difference > (3*24) ? "normal" : "urgent";
+
+  console.log(" difference > ", difference);
+
+ 
+
+
+
+
+console.log(distanceToNow)  
+
+  /*
+
+  const distance = formatDistanceToNow(deadline, {
+    locale: tr,
+    addSuffix: true,
+  });
+
+  const today = new Date();
+
+  function getClassName(date) {
+    const difference = differenceInDays(date, today);
+
+    return difference > 3 ? "normal" : "urgent";
+  }
+  const newcl= getClassName(deadline);
+  */
+
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">son teslim: <span className={difference > 3 * 24 ? 'normal' : 'urgent'}  >{distanceToNow}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
